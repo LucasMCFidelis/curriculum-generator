87ac66fef36c5e9736fd51831a5abd2072e4761b
Lucas Fidelis <lucasm241301@gmail.com>
2025-05-18 23:17:45 -0300

    feat(skill): adicionar schema de validação para criação de skill

--- backend/src/schemas/skillSchemas.ts
+++ backend/src/schemas/skillSchemas.ts
@@ -1,1 +1,18 @@
 import z from "zod";
+
+export const createSkillSchema = z.object({
+  skillTitle: z
+    .string()
+    .min(1, "O título é obrigatório.")
+    .max(100, "O título deve ter no máximo 100 caracteres."),
+  skillDescription: z
+    .string()
+    .max(250, "A descrição deve ter no máximo 250 caracteres.")
+    .optional(),
+  skillType: z
+    .string()
+    .min(1, "O tipo é obrigatório.")
+    .max(100, "O tipo deve ter no máximo 100 caracteres."),
+});
+
+export type CreateSkillDTO = z.infer<typeof createSkillSchema>;
