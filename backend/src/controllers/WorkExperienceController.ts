b6bf7da0270425e026180dbd49451e6eca2769d9
Lucas Fidelis <lucasm241301@gmail.com>
2025-05-24 10:56:15 -0300

    feat(work-experience): adicionar funcionalidade de criação de experiência profissional

--- backend/src/controllers/WorkExperienceController.ts
+++ backend/src/controllers/WorkExperienceController.ts
@@ -1,6 +1,18 @@
+import { FastifyRequest, FastifyReply } from "fastify";
 import { BaseCrud } from "../BaseCrud";
 import { WorkExperienceService } from "../services/WorkExperienceService";
+import { errorHandler } from "../utils/errorHandler";
+import { CreateWorkExperienceDTO } from "../schemas/workExperienceSchemas";
 
 const workExperienceService = new WorkExperienceService();
 
-export class WorkExperienceController extends BaseCrud {}
+export class WorkExperienceController extends BaseCrud {
+    public async create(request: FastifyRequest<{Querystring: {userId: string}, Body: CreateWorkExperienceDTO}>, reply: FastifyReply) {
+        try {
+            const newWorkExperience = await workExperienceService.createWorkExperience({...request.body, workExperienceUserId: request.query.userId})
+            return reply.status(200).send(newWorkExperience)
+        } catch (error) {
+            return errorHandler(error, reply)
+        }
+    }
+}
--- backend/src/routes/workExperienceRoutes.ts
+++ backend/src/routes/workExperienceRoutes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { WorkExperienceController } from "../controllers/WorkExperienceController";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware";
 
-const workExperienceController = new WorkExperienceController()
+const workExperienceController = new WorkExperienceController();
 
-export async function workExperienceRoutes(server: FastifyInstance) {}
+export async function workExperienceRoutes(server: FastifyInstance) {
+  server.post(
+    "/",
+    { preHandler: AuthMiddleware.authenticateAndVerifyOwnership },
+    workExperienceController.create
+  );
+}
