431cb5c06a004b753246bf89f9d77b655c545873
Lucas Fidelis <lucasm241301@gmail.com>
2025-05-31 21:54:03 -0300

    style: ajuste da paleta de cores com oklch para tema claro e escuro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+
 function App() {
     return (
     <>
       <h1>Teste</h1>
+      <Button >ola</Button>
+      <Input></Input>
     </>
   )
 }
