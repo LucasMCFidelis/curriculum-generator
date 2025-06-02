807889281731721b198e05d311ec9e6cba52fca7
Lucas Fidelis <lucasm241301@gmail.com>
2025-06-02 16:41:39 -0300

    refactor: remove componentes temporários criados para testes com ShadCN

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from "./components/ui/button"
-import { Input } from "./components/ui/input"
-
 function App() {
-    return (
+  return (
     <>
       <h1>Teste</h1>
-      <Button >ola</Button>
-      <Input></Input>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
