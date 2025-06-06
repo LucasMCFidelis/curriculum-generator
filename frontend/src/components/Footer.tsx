a14b65f1d26055758dfbf1442010463cb2b55322
Lucas Fidelis <lucasm241301@gmail.com>
2025-06-06 19:09:50 -0300

    style(button): ajusta estilo da variante icon com rounded-full e aplica aos botões de ícone existentes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -36,7 +36,7 @@ function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Button variant={"outline"} className="size-10 rounded-full">
+          <Button variant={"outline"} size={"icon"}>
             <Linkedin></Linkedin>
           </Button>
         </a>
@@ -45,7 +45,7 @@ function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Button variant={"outline"} className="size-10 rounded-full">
+          <Button variant={"outline"} size={"icon"}>
             <Github></Github>
           </Button>
         </a>
