2ad40274f21f45ee1a225c640b76b8a0db7435b3
Lucas Fidelis <lucasm241301@gmail.com>
2025-06-14 08:13:44 -0300

    types: tornar campos opcionais na tipagem User (userCity, userPortfolio, userGitHub, userLinkedIn, userResume)

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,10 +1,10 @@
 export type User = {
   userName: string;
   userEmail: string;
-  userCity: string;
-  userPortfolio: string;
-  userGitHub: string;
-  userLinkedIn: string;
-  userResume: string;
-  userCreatedAt: Date
+  userCity?: string;
+  userPortfolio?: string;
+  userGitHub?: string;
+  userLinkedIn?: string;
+  userResume?: string;
+  userCreatedAt: Date;
 };
