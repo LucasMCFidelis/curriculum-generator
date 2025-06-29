39a948464182ca7d8d0ae7655793bfb83345c337
Lucas Fidelis <lucasm241301@gmail.com>
2025-06-29 20:10:24 -0300

    feat(modal): criar componente genérico ConfirmDeleteItemModal para reutilização em exclusões

---
+++ frontend/src/components/ConfirmDeleteItem.tsx
@@ -0,0 +1,58 @@
+import { useModal } from "@/contexts/ModalContext";
+import Modal from "./modal";
+import { Trash2, X } from "lucide-react";
+import LoadingSpin from "./LoadingSpin";
+import type { UseMutationResult } from "@tanstack/react-query";
+
+interface ConfirmDeleteItemModalProps {
+  itemType: string;
+  itemId: string;
+  itemTitle: string;
+  deleteMutation: UseMutationResult<void, Error, string>;
+}
+
+function ConfirmDeleteItemModal({
+  itemType,
+  itemId,
+  itemTitle,
+  deleteMutation,
+}: ConfirmDeleteItemModalProps) {
+  const { closeModal } = useModal();
+
+  return (
+    <Modal.Root>
+      <Modal.Header>
+        <h3>
+          Confirmar exclusão de {itemType}{" "}
+          <strong className="text-destructive">{itemTitle}</strong> ?
+        </h3>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="grid md:grid-cols-2 gap-4">
+          <Modal.Close
+            disabled={deleteMutation.isPending}
+            closeAction={() => closeModal()}
+          >
+            Cancelar <X />
+          </Modal.Close>
+          <Modal.Confirm
+            disabled={deleteMutation.isPending}
+            confirmAction={() => deleteMutation.mutate(itemId)}
+          >
+            {deleteMutation.isPending ? (
+              <>
+                Excluindo... <LoadingSpin />
+              </>
+            ) : (
+              <>
+                Excluir <Trash2 />
+              </>
+            )}
+          </Modal.Confirm>
+        </div>
+      </Modal.Body>
+    </Modal.Root>
+  );
+}
+
+export default ConfirmDeleteItemModal;
