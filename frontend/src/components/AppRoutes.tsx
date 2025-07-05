b4dda8b6154a00a5f6e419ec60a9c295e6110887
Lucas Fidelis <lucasm241301@gmail.com>
2025-07-04 22:18:05 -0300

    refactor: mover SkillProvider e WorkExperienceProvider para envolverem apenas suas rotas específicas

--- frontend/src/components/AppRoutes.tsx
+++ frontend/src/components/AppRoutes.tsx
@@ -3,6 +3,8 @@ import { SkillsSection } from "./skills/SkillsSection";
 import { WorkExperienceSection } from "./workExperiences/WorkExperienceSection";
 import { LoadingSpin } from "./LoadingSpin";
 import { sectionsList } from "@/utils/sectionsList";
+import { SkillProvider } from "@/contexts/SkillContext";
+import { WorkExperienceProvider } from "@/contexts/WorkExperienceContext";
 
 export function AppRoutes() {
   return (
@@ -12,10 +14,21 @@ export function AppRoutes() {
         element={<Navigate to={sectionsList.home.href ?? "/home"} replace />}
       />
       <Route path={sectionsList.home.href} element={<LoadingSpin />} />
-      <Route path={sectionsList.skills.href} element={<SkillsSection />} />
+      <Route
+        path={sectionsList.skills.href}
+        element={
+          <SkillProvider>
+            <SkillsSection />
+          </SkillProvider>
+        }
+      />
       <Route
         path={sectionsList.experience.href}
-        element={<WorkExperienceSection />}
+        element={
+          <WorkExperienceProvider>
+            <WorkExperienceSection />
+          </WorkExperienceProvider>
+        }
       />
     </Routes>
   );
