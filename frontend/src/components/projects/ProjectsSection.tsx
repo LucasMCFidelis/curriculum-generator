4148f8645dcd0ccb5b6427bb199527a1f88c290b
Lucas Fidelis <lucasm241301@gmail.com>
2025-07-05 22:36:25 -0300

    feat(projects): adicionar dados do usuário e controle de erro no contexto de projetos
    - Adicionados campos ao ProjectContext: errorMessage, setErrorMessage, projectsUser, isLoadingProjects, isErrorProjects, refetchProjects
    - Implementado uso desses dados na ProjectsSection com estados de loading, erro e listagem

--- frontend/src/components/projects/ProjectsSection.tsx
+++ frontend/src/components/projects/ProjectsSection.tsx
@@ -1,9 +1,134 @@
+import { useProjects } from "@/hooks/useProjects";
 import { SectionsList } from "@/utils/SectionsList";
+import { Feedback } from "../feedback";
+import { Button } from "../ui/button";
+import { PenBox, RefreshCcw, Trash2 } from "lucide-react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { DateDisplay } from "../DateDisplay";
+import { getFormattedDuration } from "@/utils/getFormattedDuration";
 
 export function ProjectsSection() {
+  const {
+    setCurrentProject,
+    errorMessage,
+    projectsUser,
+    isLoadingProjects,
+    isErrorProjects,
+    refetchProjects,
+  } = useProjects();
+
   return (
-    <section id={SectionsList.projects.id}>
+    <section id={SectionsList.projects.id} className="space-y-4">
       <h2>{SectionsList.projects.label}</h2>
+
+      {isErrorProjects && (
+        <Feedback.Root>
+          <Feedback.Error message={errorMessage} />
+          <Button className="w-full sm:w-fit" onClick={() => refetchProjects()}>
+            Recarregar Projetos <RefreshCcw />
+          </Button>
+        </Feedback.Root>
+      )}
+
+      {isLoadingProjects && (
+        <Feedback.Root>
+          <Feedback.Loading message="Carregando Projetos" />
+        </Feedback.Root>
+      )}
+
+      {!isErrorProjects && !isLoadingProjects && projectsUser && (
+        <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+          {projectsUser.map((project) => (
+            <Card key={project.projectId}>
+              <CardHeader className="grid grid-rows-1 items-center">
+                <CardTitle>
+                  <h3>{project.projectTitle}</h3>
+                </CardTitle>
+                <CardAction>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => {
+                      setCurrentProject(project);
+                    }}
+                  >
+                    <PenBox />
+                  </Button>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => {
+                      setCurrentProject(project);
+                    }}
+                  >
+                    <Trash2 className="text-destructive" />
+                  </Button>
+                </CardAction>
+              </CardHeader>
+              <CardContent className="flex-1">
+                <CardDescription>
+                  <p>{project.projectDescription}</p>
+                  {project.projectRepository && (
+                    <p>
+                      <strong>Repositório: </strong>
+                      <Button variant={"link"} className="p-0 h-fit" asChild>
+                        <a
+                          href={project.projectRepository}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          {project.projectRepository}
+                        </a>
+                      </Button>
+                    </p>
+                  )}
+                  {project.projectDeploy && (
+                    <p>
+                      <strong>Deploy: </strong>
+                      <Button variant={"link"} className="p-0 h-fit" asChild>
+                        <a
+                          href={project.projectRepository}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          {project.projectDeploy}
+                        </a>
+                      </Button>
+                    </p>
+                  )}
+                  <p>
+                    <strong>Período: </strong>
+                    <DateDisplay date={project.projectStartDate} /> -{" "}
+                    {project.projectFinished && project.projectEndDate ? (
+                      <DateDisplay date={project.projectEndDate} />
+                    ) : (
+                      "Até o momento"
+                    )}
+                  </p>
+                  <p>
+                    <strong>Duração:</strong>{" "}
+                    {getFormattedDuration(
+                      project.projectStartDate,
+                      project.projectEndDate
+                    )}
+                  </p>
+                </CardDescription>
+              </CardContent>
+              <CardFooter>
+                <p>
+                  Criada em: <DateDisplay date={project.projectCreatedAt} />
+                </p>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
     </section>
   );
 }
--- frontend/src/contexts/ProjectContext.tsx
+++ frontend/src/contexts/ProjectContext.tsx
@@ -1,21 +1,62 @@
+import { api } from "@/api";
+import { useAuth } from "@/hooks/useAuth";
 import type { Project } from "@/types/Project";
+import { handleAxiosFormError } from "@/utils/handleAxiosFormError";
+import { useQuery } from "@tanstack/react-query";
 import { createContext, useState, type ReactNode } from "react";
 
 type ProjectContextType = {
   currentProject: Project | null;
   setCurrentProject: (project: Project | null) => void;
+  errorMessage: string;
+  setErrorMessage: (message: string) => void;
+  projectsUser?: Project[];
+  isLoadingProjects: boolean;
+  isErrorProjects: boolean;
+  refetchProjects: () => void;
 };
 
 export const ProjectContext = createContext({} as ProjectContextType);
 
 export function ProjectProvider({ children }: { children: ReactNode }) {
+  const { currentUser } = useAuth();
   const [currentProject, setCurrentProject] = useState<Project | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const {
+    data: projectsUser,
+    isLoading: isLoadingProjects,
+    isError: isErrorProjects,
+    refetch: refetchProjects,
+  } = useQuery<Project[], Error>({
+    queryKey: ["projects"],
+    queryFn: async () => {
+      try {
+        const response = await api.get(
+          `/users?userId=${currentUser?.userId}&userProjects=true`
+        );
+        return response.data.userProjects;
+      } catch (error) {
+        handleAxiosFormError({
+          error,
+          setError: setErrorMessage,
+          genericMessage: "Erro ao carregar projetos",
+        });
+      }
+    },
+  });
 
   return (
     <ProjectContext.Provider
       value={{
         currentProject,
         setCurrentProject,
+        errorMessage,
+        setErrorMessage,
+        projectsUser,
+        isLoadingProjects,
+        isErrorProjects,
+        refetchProjects,
       }}
     >
       {children}
